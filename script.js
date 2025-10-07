bfc4eaa615f7d66aa6f36b15bee4cb4f0d34e48f
saikat <saikat@macs-MacBook-Pro.local>
2025-10-08 03:04:16 +0600

    n/a

--- script.js
+++ script.js
@@ -7,10 +7,8 @@ function updateClock(){
     hours = hours < 10 ? "0" + hours : hours;
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
-
     document.getElementById("clock").textContent = `${hours}:${minutes}:${seconds}`;
 
 }
-
 updateClock();
 setInterval(updateClock,1000);
